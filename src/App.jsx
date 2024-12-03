3b7627a810b5b7a3334368f2e5909e66a0098a61
Iriya-n <nikonov.is@yandex.ru>
2024-12-03 06:26:56 +0400

    modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.jsx 	new file:   src/api/api.js 	new file:   src/assets/card-image.png 	new file:   src/assets/left.svg 	new file:   src/assets/logo.png 	deleted:    src/assets/react.svg 	new file:   src/assets/right.svg 	new file:   src/components/card/Card.jsx 	new file:   src/components/card/Card.module.scss 	new file:   src/components/footer/Footer.jsx 	new file:   src/components/footer/Footer.module.scss 	new file:   src/components/header/Header.jsx 	new file:   src/components/header/Header.module.scss 	new file:   src/hooks/useAllCharacters.js 	modified:   src/index.css 	modified:   src/main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Header } from "./components/header/Header.jsx"
+import { Card } from "./components/card/Card.jsx"
+import { Footer } from "./components/footer/Footer.jsx"
+import { useCharacters } from "./hooks/useAllCharacters.js"
+import { useState } from "react"
+import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [page, setPage] = useState(1)
+  const { data: charactersData, isFetched, isLoading } = useCharacters({ page })
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Header />
+      <div className="wrapper">
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : isFetched ? (
+          charactersData?.map(el => (
+            <Card key={el.id} data={el} />
+          ))
+        ) : (
+          <p>Fetching...</p>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer page={page} setPage={setPage} />
     </>
   )
 }
-
-export default App
